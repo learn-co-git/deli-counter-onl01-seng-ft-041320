b81ec29df52196f51a2971bf47ef5595586909ab
Neil Empey <argus.two.2@gmail.com>
2020-04-15 03:40:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def line(katz_deli)
+	str = ""
+	katz_deli.each_with_index do |ele, i|
+		number = i + 1
+		str += number.to_s + ". "  + ele + " "
+	end
+	puts "The line is currently: " + str
+end
+
+def take_a_number(katz_deli, name)
+	katz_deli << name
+	puts "Welcome #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+	if(katz_deli.length == 0)
+		return "There is nobody waiting to be served!"
+	else
+		puts "Currently serving #{katz_deli[0]}"
+		katz_deli.shift()
+	end
+end
